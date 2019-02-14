e3bc60586f05e8ecbfdd528eb4c97b41a412a329
Yusuf Britton <yusufbritton@yahoo.com>
2019-02-14 05:51:10 -0500

    updated admin page to edit blog post, deleted dummy data from views

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,9 @@
 from django.shortcuts import render #This directs to <root>/templates
 from .models import Post #the dot means the file is in the current folder
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
-
-
-
 #view.home ran from urls.py in blog folder.
+# for the date filter, check out django documentation
+# https://docs.djangoproject.com/en/2.0/ref/templates/builtins/#date
 def home(request):
     context = {
         'posts': Post.objects.all()

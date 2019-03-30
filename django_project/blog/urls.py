46fe03ddfd0b4d2a038ea43e6aede21a1813d6ce
Yusuf Britton <yusufbritton@yahoo.com>
2019-03-30 06:51:53 -0400

    Created PostListView to display post on blog and show most recent posts first

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
+from .views import PostListView
 from . import views #. is for the current directory
 
 #after being called from django_project blog.urls, it will come to this folder
 #and run a path with an empty string. when it runs it, it will invoke
 #views.home
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
     path('about/', views.about, name='blog-about'),
 ]
+
+"""
+
+Reason for error when using PostListView.as_view() looking for a view by:
+
+<app>/<model>_<viewtype>.html
+"""
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render #This directs to <root>/templates
+from django.views.generic import ListView
 from .models import Post #the dot means the file is in the current folder
 
 #view.home ran from urls.py in blog folder.
@@ -10,6 +11,28 @@ def home(request):
     }
     # context variable is accessing posts dictionary
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    """
+    arg:
+        ListView: inherted from django.
+    input:
+        model: This is accessing the post data
+
+        template_name: This is directing the post data to the home page as its
+        template
+
+        context_object_name: this is setting the variable to be called "posts"
+        this will loop over all the post information
+
+        ordering: this will show the most recent post at the top of a page.
+        Note: if you want to do newest to oldest, put minus sign
+    output:
+    """
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ["-date_posted"]
 
 def about(request):
     return render(request, 'blog/about.html', {'title':'about'})

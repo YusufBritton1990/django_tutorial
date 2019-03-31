d663577cf0e24fea9e8d4d78555a2eda11a073f1
Yusuf Britton <yusufbritton@yahoo.com>
2019-03-31 07:53:22 -0400

    Using class view, can view individual posts from blog page

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,12 +1,23 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView
 from . import views #. is for the current directory
 
-#after being called from django_project blog.urls, it will come to this folder
-#and run a path with an empty string. when it runs it, it will invoke
-#views.home
+"""
+urlpatterns
+after being called from django_project blog.urls, it will come to this folder
+and run a path with an empty string. when it runs it, it will invoke
+views.home
+
+NOTE: views.home replaced with PostDetailView.as_view, which is showing
+Post
+
+blog-home: using PostDetailView class, this page contains the post
+post-detail: based on information
+blog-about: routes to about page using views.about function
+"""
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about'),
 ]
 
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render #This directs to <root>/templates
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post #the dot means the file is in the current folder
 
 #view.home ran from urls.py in blog folder.
 # for the date filter, check out django documentation
 # https://docs.djangoproject.com/en/2.0/ref/templates/builtins/#date
+
+# NOTE: Currently being replaced with PostListView class to view post
 def home(request):
     context = {
         'posts': Post.objects.all()
@@ -28,11 +30,26 @@ class PostListView(ListView):
         ordering: this will show the most recent post at the top of a page.
         Note: if you want to do newest to oldest, put minus sign
     output:
+        Displays posts on blog site, showing the most recent first
     """
     model = Post
     template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ["-date_posted"]
+
+class PostDetailView(DetailView):
+    """
+    arg:
+        DetailView: inherted from django.
+    input:
+        model: This is accessing the post data
+
+    output:
+        Display individual post when clicked. This data is routed into
+        template post_detail.html which is dynamically populating the post
+    """
+    model = Post
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title':'about'})

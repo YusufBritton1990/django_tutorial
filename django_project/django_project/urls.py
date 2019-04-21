de4fb83fa9d5dad019d4d25b4009dfec82cfb047
Yusuf Britton <yusufbritton@yahoo.com>
2019-04-21 08:25:17 -0400

    creating password reset page, confirmation page, and complete alert routes

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -28,12 +28,40 @@ from users import views as user_views #import view directly into project
 #Once a user type it blog, it will screch in blog>urls for futher processing
 # For logout path, if no template_name is set, it will default to using django's admin page
 
+"""
+paths:
+    admin: connection to admin site. default route
+    register: sign up a new user. function view
+    profile: look at logged in user profile. function view
+    login: . Class view
+    logout: . Class view
+    password_reset. Class view
+    password_reset_done: success message, stating email was sent. Class view
+    password-reset-confirm: confirm the user and security token. Class view
+    '': blog home page. this page is the index
+"""
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'),
+    name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'),
+    name='logout'),
+
+    path('password-reset/',
+    auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
+    name='password_reset'),
+
+    path('password-reset/done',
+    auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+    name='password_reset_done'),
+
+    path('password-reset-confirm/<uid64>/<token>',
+    auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+    name='password_reset_confirm'),
+
     path('', include('blog.urls')),
 ]
 
